45861ee87a8127378d4caaf7cc736aef738273a9
“yan_xh” <1540719909@qq.com>
2026-07-09 10:58:58 +0800

    功能： 联网检索节点（node_web_search_mcp）的业务代码及单元测试

--- app/process/query/agent/nodes/node_web_search_mcp.py
+++ app/process/query/agent/nodes/node_web_search_mcp.py
@@ -10,8 +10,18 @@ def node_web_search_mcp(state):
     节点功能：调用外部搜索引擎补充信息
     """
     add_running_task(state["session_id"], sys._getframe().f_code.co_name, state["is_stream"])
-    state = search_by_web(state)
+    web_search_docs = search_by_web(state)
     add_done_task(state["session_id"], sys._getframe().f_code.co_name, state["is_stream"])
     return {
-        "web_search_docs":[]
+        "web_search_docs": web_search_docs
     }
+
+
+if __name__ == "__main__":
+    test_state = {
+        "session_id": "xxxx",
+        "is_stream": False,
+        "rewritten_query": "HAK 180 在出厂默认状态下，若想在纸张上只把烫金膜转印到顶部 50 mm–170 mm 的局部区域，应在操作面板上如何设置",
+    }
+    result_state = node_web_search_mcp(test_state)
+    print(result_state)
--- app/rag/query/web_search_service.py
+++ app/rag/query/web_search_service.py
@@ -1,8 +1,52 @@
-import time
+import asyncio
+import json
 
+from agents.mcp import MCPServerStreamableHttp
+
+from app.infra.config.providers import infra_config
 from app.process.query.agent.state import QueryGraphState
+from app.shared.runtime.logger import logger, step_log
 
+@step_log("get_data_and_validate")
+def get_data_and_validate(state):
+    rewritten_query = state.get("rewritten_query")
+    if not rewritten_query:
+        logger.error(f"重写的问题为空,业务无法继续,提前终止!")
+        raise ValueError(f"重写的问题为空,业务无法继续,提前终止!")
+    return rewritten_query
 
+@step_log("open_ai_mcp")
+async def open_ai_mcp(rewritten_query):
+    # 1. 链接mcp服务
+    mcp_server = MCPServerStreamableHttp(
+        name="Streamable HTTP Python Server",
+        params={
+            "url": infra_config.mcp_config.mcp_base_url,
+            "headers": {"Authorization": f"Bearer {infra_config.mcp_config.api_key}"},
+            "timeout": 50,
+        },
+        cache_tools_list=True,
+        max_retry_attempts=3,
+    )
+    try:
+        # 2.mcp服务链接
+        await mcp_server.connect()
+        # 3. mcp工具调用
+        mcp_result = await mcp_server.call_tool(
+            tool_name="bailian_web_search",
+            arguments={
+                "query": rewritten_query,
+                "count": 5
+            }
+        )
+        return mcp_result
+    except Exception as e:
+        logger.exception(f"mcp调用发生问题,问题:{str(e)}")
+    finally:
+        # 4. 清空链接
+        await mcp_server.cleanup()
+
+@step_log("search_by_web")
 def search_by_web(state: QueryGraphState) -> QueryGraphState:
     """
     网络搜索服务：
@@ -11,5 +55,13 @@ def search_by_web(state: QueryGraphState) -> QueryGraphState:
     3. 包含标题、链接和摘要
     4. 回写 web_search_docs
     """
-    time.sleep(0.5)
-    return state
+
+    # 3. 获取并校验参数(state) -> rewritten_query
+    rewritten_query = get_data_and_validate(state)
+    # 4. async 使用openai提供mcp方式进行调用(rewritten_query) -> 查询结果
+    mcp_result = asyncio.run(open_ai_mcp(rewritten_query))
+    # 5. 结果解析 todo 注意: 外层都是属性,不是字典
+    result_str = mcp_result.content[0].text
+    result_dict = json.loads(result_str)
+    web_search_docs = result_dict.get("pages",[])
+    return web_search_docs
